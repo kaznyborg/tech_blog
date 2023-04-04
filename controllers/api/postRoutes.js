350c582e485c04d6f549a469c35cc93e25b2bd75
Kaz Nyborg-Andersen <113869318+kaznyborg@users.noreply.github.com>
2023-04-04 15:24:14 -0700

    feature/js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,12 +6,95 @@ const withAuth = require('../../utils/auth');
 // TODO - create a POST route for creating a new post
 // This should be a protected route, so you'll need to use the withAuth middleware
 
+router.post('/create', withAuth, async (req, res) => {
+    const body = req.body
+    try {
+      const post = await Post.create({ 
+        title: req.body.title,
+        body: req.body.body,  
+        userid: req.session.userid, 
+      }); 
+      res.status(200).json({ message: "Posted"})
+    } 
+    catch (err) {
+      res.status(500).json(err); 
+    }
+  }); 
 
 // TODO - create a PUT route for updating a post's title or body
 // This should be a protected route, so you'll need to use the withAuth middleware
 
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const updatePost = await Post.update({ body: req.body,},
+        {
+        where: {
+          id: req.params.id
+        }
+      });
+      if(!updatePost) {
+        res.status(404).json({
+          message: 'No post found with this id'  + req.params.id});
+          return; 
+        }
+      res.status(200).json(updatePost); 
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    }); 
+
+    router.get("/:id", withAuth, async (req, res) => {
+      try {
+        const singlePost = await Post.findOne({
+          where: { id: req.params.id },
+          attributes: ["id", "body", "title", "createdAt"],
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+            {
+              model: Comment,
+              order: [["createdAt", "DESC"]],
+              attributes: ["body"],
+            },
+          ],
+        });
+        console.log(singlePost, 'SINGLE POST')
+        const post = singlePost;
+        res.render("single-post", {
+          posts: post,
+          id: req.params.id,
+          body: req.body,
+        });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
 
 // TODO - create a DELETE route for deleting a post with a specific id
 // This should be a protected route, so you'll need to use the withAuth middleware
+
+router.delete('/:id', withAuth, async (req, res) => {
+    // delete a category by its `id` value
+    try {
+    const deletePost = await Post.destroy ({
+      where: {
+      id: req.params.id
+    }
+    });
+  
+      if (!deletePost) {
+        res.status(404).json({
+          message: 'No post found with this id' });
+          return;
+      }
+  
+      res.status(200).json(deletePost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  
+  }); 
 
 module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,22 +4,55 @@
 // All of these routes will be protected by the withAuth middleware function.
 
 const router = require("express").Router();
-const { Post } = require("../models/");
+const { Post, User } = require("../models/");
 const withAuth = require("../utils/auth");
 
 // TODO - create logic for the GET route for / that renders the dashboard homepage
 // It should display all of the posts created by the logged in user
 router.get("/", withAuth, async (req, res) => {
   // TODO - retrieve all posts from the database for the logged in user
+  try {
+    const userPost = await Post.findAll({
+      where: { userId:req.session.userId },
+      order: [["createdAt", "DESC"]], 
+      include: [
+        { 
+          model: User, 
+          attributes: ['username']
+        }
+      ],
+    }); 
+    const posts = userPost.map((post) => post.get({plain:true}))
   // render the dashboard template with the posts retrieved from the database
   //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard" });
+  res.render("admin-all-posts", { layout: "dashboard", posts });
   // refer to admin-all-posts.handlebars write the code to display the posts
+} catch (error) {
+  res.redirect('/login');
+}
 });
 
+router.get('/create', withAuth, async (req, res) => {
+  res.render('admin-create-post', { layout: "dashboard",});
+
+});
+router.post('/create', withAuth, async (req, res) => {
+  try{
+  await Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    userId: req.session.userId
+  })
+  res.redirect('/dashboard');
+}catch(err){
+  res.status(500).json(err);
+}
+})
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
-
+router.get('/new', withAuth, async (req, res) => {
+  res.render('newpost', { layout: "dashboard",});
+}); 
 // TODO - create logic for the GET route for /edit/:id that renders the edit post page
 // It should display a form for editing an existing post
 
